13bb81968876750c15815239848ec0ca0a9f43db
Василий Красовский <vasiliy_krasovskiey@live.com>
2021-04-15 08:40:55 +0300

    assembly COM-видимым

--- WeigherService/Properties/AssemblyInfo.cs
+++ WeigherService/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b59265b7-0365-4fc8-83a2-2d62451495c6")]
